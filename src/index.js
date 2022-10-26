8ad7bdf58716dff0d219f9616126aa4580dd82fd
zignd <zignd.igor@gmail.com>
2022-10-26 08:08:43 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
 
 app.get('/', function (req, res) {
   res.json({
-    message: "Calculator is running",
+    message: "Calculator is running change-1",
   })
 })
 
